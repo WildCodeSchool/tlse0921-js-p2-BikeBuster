4341628dabe0f36c00f53bd647567623075210da
Cristi <mihaes_cristi@yahoo.fr>
2021-11-17 14:27:14 +0100

    page tarif

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,76 +1,87 @@
+/* eslint-disable react/jsx-no-bind */
 import { React, useState, useCallback } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
-import MenuIcon from '@mui/icons-material/Menu';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import Switch from '@mui/material/Switch';
+import { alpha, styled } from '@mui/material/styles';
+import { green } from '@mui/material/colors';
 import Map from './Map';
 import Count from './Counter';
+import SearchFrom from './SearchFrom';
+import SearchTo from './SearchTo';
+
+const GreenSwitch = styled(Switch)(({ theme }) => ({
+  '& .MuiSwitch-switchBase.Mui-checked': {
+    color: green[600],
+    '&:hover': {
+      backgroundColor: alpha(green[600], theme.palette.action.hoverOpacity),
+    },
+  },
+  '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
+    backgroundColor: green[600],
+  },
+}));
+
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
 
 function Home() {
   const [count, setCount] = useState(1);
-
-  // function handleMoreBikes() {
-  //   if (count > 1) { setCount((e) => e - 1); }
-  // }
-
-  // function handleLessBikes() {
-  //   if (count < 20) { setCount((e) => e + 1); }
-  // }
-
-  // const memoizedCallback = useCallback(
-  //   () => {
-  //     doSomething(a, b);
-  //   },
-  //   [a, b],
-  // );
 
   const handleMoreBikes = useCallback(() => {
     if (count > 1) {
       setCount((e) => e - 1);
     }
   }, [count]);
-
   const handleLessBikes = useCallback(() => {
     if (count < 20) {
       setCount((e) => e + 1);
     }
   }, [count]);
+  const [check, setCheck] = useState(false);
+
+  function filterBikeStand() {
+    setCheck(!check);
+  }
 
   return (
     <div className="globalHome">
+      <>{/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}</>
       <input id="button-search-container" type="checkbox" />
       <div className="scrolling-search">
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <>{/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}</>
         <label htmlFor="button-search-container">
-          <MenuIcon />
+          <ArrowDownwardIcon />
         </label>
       </div>
       <div className="search-container">
-        <Count
-          count={count}
-          handleMoreBikes={handleMoreBikes}
-          handleLessBikes={handleLessBikes}
-        />
+        <div className="switch-count-container">
+          <div className="switch-container">
+            <p className="take-tag">Prendre</p>
+            <GreenSwitch {...label} onClick={filterBikeStand} />
+            <p id="deposer">Déposer</p>
+          </div>
+          <div className="count-container">
+            <Count
+              count={count}
+              handleMoreBikes={handleMoreBikes}
+              handleLessBikes={handleLessBikes}
+            />
+          </div>
+        </div>
         <div className="search-bar">
-          <input
-            type="search"
-            id="searchbar"
-            name="searchbar"
-            autoComplete="address-level4"
-            placeholder="Départ"
-          />
-          <input
-            type="search"
-            id="searchbar2"
-            name="searchbar"
-            autoComplete="address-level4"
-            placeholder="Arrivée"
-          />
+          <div className="departure">
+            <SearchFrom />
+          </div>
+          <div className="arrival">
+            <SearchTo />
+          </div>
           <div className="search-icon">
             <SearchIcon sx={{ fontSize: 25 }} />
           </div>
         </div>
       </div>
-      <div>
-        <Map count={count} />
+      <div className="map">
+        <Map check={check} count={count} />
       </div>
     </div>
   );
---
+++ src/components/ResultListStand.jsx
@@ -0,0 +1,99 @@
+/* eslint-disable react/prop-types */
+import { Marker, Popup } from 'react-leaflet';
+
+import L from 'leaflet';
+
+const ResultListStand = (props) => {
+  const { results, count } = props;
+
+  const redMarker = new L.Icon({
+    iconUrl: 'redmarker.png',
+    iconSize: [50, 50],
+  });
+
+  const yellowMarker = new L.Icon({
+    iconUrl: 'yellowmarker.png',
+    iconSize: [50, 50],
+  });
+
+  const greenMarker = new L.Icon({
+    iconUrl: 'greenmarker.png',
+    iconSize: [50, 50],
+  });
+
+  return (
+    <div>
+      {results
+        .filter(
+          (station) => (
+            station.available_bike_stands - count) <= 5
+            && station.available_bike_stands >= count,
+        )
+        .map((e) => (
+          <Marker
+            key={e.name}
+            position={[e.position.lat, e.position.lng]}
+            icon={redMarker}
+          >
+            <Popup>
+              {`Station ${e.name.replace(/[0-9]/gi, '').replace('-', '')}`}{' '}
+              <br />
+              {`Vélos disponibles: ${e.available_bikes}`} <br />
+              {`Places disponibles: ${e.available_bike_stands}`} <br />
+              <a href={`https://www.google.com/maps/dir/?api=1&destination=${e.position.lat}%2C${e.position.lng}&travelmode=bicycling`} target="_blank" rel="noreferrer">
+                Itinéraire
+              </a>
+            </Popup>
+          </Marker>
+        ))}
+
+      {results
+        .filter(
+          (station) => (station.available_bike_stands - count) > 5
+              && (station.available_bike_stands - count) <= 10
+              && station.available_bike_stands >= count,
+        )
+        .map((e) => (
+          <Marker
+            key={e.name}
+            position={[e.position.lat, e.position.lng]}
+            icon={yellowMarker}
+          >
+            <Popup>
+              {`Station ${e.name.replace(/[0-9]/gi, '').replace('-', '')}`}{' '}
+              <br />
+              {`Vélos disponibles: ${e.available_bikes}`} <br />
+              {`Places disponibles: ${e.available_bike_stands}`} <br />
+              <a href={`https://www.google.com/maps/dir/?api=1&destination=${e.position.lat}%2C${e.position.lng}&travelmode=bicycling`} target="_blank" rel="noreferrer">
+                Itinéraire
+              </a>
+            </Popup>
+          </Marker>
+        ))}
+
+      {results
+        .filter(
+          (station) => (station.available_bike_stands - count) > 10
+          && station.available_bike_stands >= count,
+        )
+        .map((e) => (
+          <Marker
+            key={e.name}
+            position={[e.position.lat, e.position.lng]}
+            icon={greenMarker}
+          >
+            <Popup>
+              {`Station ${e.name.replace(/[0-9]/gi, '').replace('-', '')}`}{' '}
+              <br />
+              {`Vélos disponibles: ${e.available_bikes}`} <br />
+              {`Places disponibles: ${e.available_bike_stands}`} <br />
+              <a href={`https://www.google.com/maps/dir/?api=1&destination=${e.position.lat}%2C${e.position.lng}&travelmode=bicycling`} target="_blank" rel="noreferrer">
+                Itinéraire
+              </a>
+            </Popup>
+          </Marker>
+        ))}
+    </div>
+  );
+};
+export default ResultListStand;
