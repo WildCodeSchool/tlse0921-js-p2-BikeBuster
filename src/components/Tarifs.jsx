2316496607a14ae5570e4bec0bb6a33bf32583c4
Cristi <mihaes_cristi@yahoo.fr>
2021-11-18 14:09:35 +0100

    changement tableau mobile

--- src/components/Tarifs.jsx
+++ src/components/Tarifs.jsx
@@ -23,7 +23,6 @@ const Tarifs = () => (
               <th>Ticket 1 jour</th>
               <th>Ticket 7 jour</th>
               <th>Abonnement 1 mois</th>
-              <th>Abonnement 1 an</th>
             </tr>
           </thead>
           <tbody className="body">
@@ -32,39 +31,33 @@ const Tarifs = () => (
               <td>1,20€</td>
               <td>5€</td>
               <td>10€</td>
-              <td>25€</td>
             </tr>
             <tr>
               <td>VéloToulouse Pastel</td>
               <td>/</td>
               <td>/</td>
               <td>10€</td>
-              <td>20€</td>
             </tr>
             <tr>
               <td>VélÔToulouse montoulouse et montoulouse Sénior</td>
               <td>/</td>
               <td>/</td>
               <td>/</td>
-              <td>20€</td>
             </tr>
             <tr>
               <td>VéloToulouse classique</td>
               <td>1,20€</td>
               <td>5€</td>
               <td>10€</td>
-              <td>25€</td>
             </tr>
             <tr>
               <td>VélÔToulouse Etudiants MUT</td>
               <td>/</td>
               <td>/</td>
               <td>/</td>
-              <td>20€</td>
             </tr>
             <tr>
               <td>La 1ère demi-heure</td>
-              <td>Gratuit</td>
               <td>Gratuit</td>
               <td>Gratuit</td>
               <td>Gratuit</td>
@@ -74,19 +67,16 @@ const Tarifs = () => (
               <td>0,5€</td>
               <td>0,5€</td>
               <td>0,5€</td>
-              <td>0,5€</td>
             </tr>
             <tr>
               <td>Entre 1h et 2h</td>
               <td>1,50€</td>
               <td>1€</td>
               <td>1€</td>
-              <td>1€</td>
             </tr>
             <tr>
               <td>Au-delà de 2h / par heure suivante</td>
               <td>2€</td>
-              <td>1€</td>
               <td>1€</td>
               <td>1€</td>
             </tr>
@@ -103,10 +93,11 @@ const Tarifs = () => (
           <img src={image} alt="image2" />
           <h2 className="card-title1">Abonnement mensuel</h2>
           <p>
-            Souple, l&apos;abonnement mensuel vous permet d&apos;utiliser le service selon
-            vos besoins tout en profitant des avantages de la carte d&apos;abonné.
-            Celle-ci vous permet de prendre un vélo rapidement et de vous
-            déplacer d’une station à l&apos;autre en quelques minutes.
+            Souple, l&apos;abonnement mensuel vous permet d&apos;utiliser le
+            service selon vos besoins tout en profitant des avantages de la
+            carte d&apos;abonné. Celle-ci vous permet de prendre un vélo
+            rapidement et de vous déplacer d’une station à l&apos;autre en
+            quelques minutes.
           </p>
           <br />
           <br />
@@ -130,10 +121,10 @@ const Tarifs = () => (
           <img src={velotoulouse} alt="image2" />
           <h2 className="card-title2">Abonnement annuel</h2>
           <p>
-            Simple d&apos;utilisation, l&apos;abonnement annuel vous permet de louer un
-            vélo rapidement. Il suffit de passer sa carte directement sur le
-            lecteur de la borne. Vous pouvez ainsi vous déplacer d&apos;une station à
-            l&apos;autre en quelques minutes.
+            Simple d&apos;utilisation, l&apos;abonnement annuel vous permet de
+            louer un vélo rapidement. Il suffit de passer sa carte directement
+            sur le lecteur de la borne. Vous pouvez ainsi vous déplacer
+            d&apos;une station à l&apos;autre en quelques minutes.
             <br />
             <br />
           </p>
